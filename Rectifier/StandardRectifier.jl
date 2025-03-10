70acd9390611990cdd7eeca7bb4796e678685d51
filomba01 <f.balzarini@an4a.com>
2025-03-10 14:04:02 +0100

    implemented standard rectification

--- Rectifier/StandardRectifier.jl
+++ Rectifier/StandardRectifier.jl
@@ -1,9 +1,9 @@
 module StandardRectifier
     include("Rectifier.jl")
     import .RectifierModule: rectify, Conics, Homography, Conic, conic_par2alg
-    #using NonlinearSolve
     using SymPy
     using LinearAlgebra
+    
     # Extend the function from Main.RectifierModule using its absolute qualified name
     function rectify(C_img::Conics)::Homography
         @info "Rectifying using SymPy"
@@ -21,9 +21,9 @@ module StandardRectifier
         eq2 = Eq(a2*x^2 + b2*x*y + c2*y^2 + d2*x + e2*y + f2, 0)
 
         solutions = solve((eq1, eq2), (x, y))
-        @info "Solutions: $solutions"
-        II = [N(solutions[1][1]); N(solutions[1][2])]
-        JJ = [N(solutions[2][1]); N(solutions[2][2])]
+        @debug "Solutions: $solutions"
+        II = [N(solutions[1][1]); N(solutions[1][2]); 1]
+        JJ = [N(solutions[2][1]); N(solutions[2][2]); 1]
         @debug "II: $II"
         @debug "JJ: $JJ"
 
@@ -32,29 +32,16 @@ module StandardRectifier
         imDCCP = imDCCP./norm(imDCCP)
         @debug "imDCCP: $imDCCP"
 
-        # extract the line at the infinity (2fix)
-        #l_inf = nullspace(imDCCP)
-        #@info "l_inf: $l_inf"
-
-        #H_1 = Homography([I(2) zeros(2,1); l_inf])
-        #@info "H_1: $H_1"
-
 
         U, S, V = svd(imDCCP)
-        @info "U: $U"
-        @info "S: $S"
-        @info "V: $V"
+        @debug "U: $U"
+        @debug "S: $S"
+        @debug "V: $V"
 
-        t =  Diagonal(1 ./ sqrt.(S)) * U'
+        H =  Diagonal(1 ./ sqrt.(S)) * U'
 
-        H_2 = [t zeros(2,1); zeros(1,2) 1]
-        @info "H_2: $H_2"
+        @debug "H: $H"
 
-        #H = H_1*H_2
-        #@info "H: $H"
-        
-        # Do something with the solution
-        # For now, just return a random homography
-        return Homography(rand(3,3))
+        return Homography(H)
     end
 end
