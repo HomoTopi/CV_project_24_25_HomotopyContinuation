01e1c1e60d6a45a9d894f992e7775fa8c74aa8a0
filomba01 <f.balzarini@an4a.com>
2025-03-13 16:20:33 +0100

    updated example

--- Rectifier/example.jl
+++ Rectifier/example.jl
@@ -1,9 +1,11 @@
 include("StandardRectifier.jl")
 include("HomotopyRectifier.jl")
 
-import .StandardRectifier: rectify, Conics, Homography
-import .HomotopyContinuationRectifier: rectify as rectify2, Conics as Conics2, Homography as Homography2
-C_img = Conics(rand(3,3), rand(3,3))
+import .StandardRectifier: rectify, Conics, Homography, Conic
+#import .HomotopyContinuationRectifier: rectify as rectify2, Conics as Conics2, Homography as Homography2
+C1 = Conic(rand(3,3))
+C2 = Conic(rand(3,3))
+C_img = Conics(C1, C2)
 try
     H = rectify(C_img)
     println("Homography: $H")
@@ -11,10 +13,10 @@ catch e
     println("Error: $e")
 end
 
-try
-    C_img2 = Conics2(rand(3,3), rand(3,3))
-    H2 = rectify2(C_img2)
-    println("Homography: $H2")
-catch e
-    println("Error: $e")
-end
+#try
+#    C_img2 = Conics2(C1, C2)
+#    H2 = rectify2(C_img2)
+#    println("Homography: $H2")
+#catch e
+#    println("Error: $e")
+#end
