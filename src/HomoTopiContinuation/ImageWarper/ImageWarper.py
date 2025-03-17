5b08813ceb54d3dc54711e2bffc29be3c1a15c2d
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-03-17 16:19:22 +0100

    feat: Image Warper python version

---
+++ src/HomoTopiContinuation/ImageWarper/ImageWarper.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2
+from HomoTopiContinuation.DataStructures.datastructures import Homography
+
+
+class ImageWarper:
+    """
+    This class is used to warp an image using a homography matrix.
+    """
+
+    def __call__(self, img: np.ndarray, H: Homography) -> np.ndarray:
+        """
+        Warp an image using a homography matrix.
+
+        Args:
+            img (_type_): The image to warp
+            H (Homography): The homography matrix
+
+        Returns:
+            np.ndarray: The warped image
+        """
+        # Get the shape of the image
+        h, w = img.shape[:2]
+
+        # Warp the image
+        warped_img = cv2.warpPerspective(img, H.H, (w, h))
+
+        return warped_img
