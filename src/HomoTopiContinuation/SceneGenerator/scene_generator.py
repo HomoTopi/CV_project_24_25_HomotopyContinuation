540aa32f4e55e43cf1d7ed7f4fc2d2116ebe248b
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-03-20 18:50:14 +0100

    chore: remove legacy plotter reference

--- src/HomoTopiContinuation/SceneGenerator/scene_generator.py
+++ src/HomoTopiContinuation/SceneGenerator/scene_generator.py
@@ -23,9 +23,6 @@ class SceneGenerator:
         # Convert circles to conics
         C1_true = scene_description.circle1.to_conic()
         C2_true = scene_description.circle2.to_conic()
-        plotter = Plotter()
-        # Plot the true conics
-        plotter.plot_conics(Conics(C1_true, C2_true), "True Conics")
         # Compute the homography
         H = SceneGenerator.compute_H(scene_description)
         H_inv = H.inv()
@@ -33,8 +30,6 @@ class SceneGenerator:
         C1 = Conic(H_inv.T @ C1_true.M @ H_inv)
         C2 = Conic(H_inv.T @ C2_true.M @ H_inv)
         conics = Conics(C1, C2)
-        # Plot the transformed conics
-        plotter.plot_conics(conics, "Transformed Conics")
         return Img(H, conics)
 
     @staticmethod
