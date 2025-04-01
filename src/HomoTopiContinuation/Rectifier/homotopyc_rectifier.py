fix: computation of imDCCP

src/HomoTopiContinuation/Rectifier/homotopyc_rectifier.py
@@ -17,15 +17,16 @@ class HomotopyContinuationRectifier(Rectifier):
     This rectifier solves a system of conic equations symbolically to find intersection
     points, then computes the homography.
     """
-    
+
     def __init__(self):
         # add the HomotopyContinuation package
-        juliapkg.add("HomotopyContinuation","f213a82b-91d6-5c5d-acf7-10f1c761b327")
+        juliapkg.add("HomotopyContinuation",
+                     "f213a82b-91d6-5c5d-acf7-10f1c761b327")
         juliapkg.resolve()
-                
+
         # call the __init__ of the parent class
         super().__init__()
-        
+
     def rectify(self, C_img: Conics) -> Homography:
         """
         Rectify a pair of conics using SymPy.
@@ -36,38 +37,41 @@ class HomotopyContinuationRectifier(Rectifier):
         Returns:
             Homography: The rectification homography
         """
-        
+
         script = self._get_script('Julia/rectify.jl')
         self.logger.info("Rectifying using HomotopyContinuation.jl")
         self.logger.debug(f"Conics: {C_img}")
-        
+
         self.logger.info(f"Evaluating script: {script}")
 
         # Extract algebraic parameters from conics
         a1, b1, c1, d1, e1, f1 = C_img.C1.to_algebraic_form()
         a2, b2, c2, d2, e2, f2 = C_img.C2.to_algebraic_form()
         a3, b3, c3, d3, e3, f3 = C_img.C3.to_algebraic_form()
-        
-        jl.a1, jl.b1, jl.c1, jl.d1, jl.e1, jl.f1 = np.array([a1, b1, c1, d1, e1, f1])
-        jl.a2, jl.b2, jl.c2, jl.d2, jl.e2, jl.f2 = np.array([a2, b2, c2, d2, e2, f2])
-        jl.a3, jl.b3, jl.c3, jl.d3, jl.e3, jl.f3 = np.array([a3, b3, c3, d3, e3, f3])
-        
+
+        jl.a1, jl.b1, jl.c1, jl.d1, jl.e1, jl.f1 = np.array(
+            [a1, b1, c1, d1, e1, f1])
+        jl.a2, jl.b2, jl.c2, jl.d2, jl.e2, jl.f2 = np.array(
+            [a2, b2, c2, d2, e2, f2])
+        jl.a3, jl.b3, jl.c3, jl.d3, jl.e3, jl.f3 = np.array(
+            [a3, b3, c3, d3, e3, f3])
+
         jl.seval(script)
-        ## TODO: check if casting is the same as the one used in SymPy
-        solutions = np.array([ np.complex128(sol) for sol in jl.sol])
-        #real_solutions = np.array([ np.float64(sol) for sol in jl.real_sol])
+        # TODO: check if casting is the same as the one used in SymPy
+        solutions = np.array([np.complex128(sol) for sol in jl.sol])
+        # real_solutions = np.array([ np.float64(sol) for sol in jl.real_sol])
         self.logger.info(f"Result: {solutions}")
-        #self.logger.info(f"Real solutions: {real_solutions}")
-        ## TODO: extract the intersection points from the result
-        ## TODO: compute the rectification homography both by svd and by a fully homotopy continuation approach
-        
+        # self.logger.info(f"Real solutions: {real_solutions}")
+        # TODO: extract the intersection points from the result
+        # TODO: compute the rectification homography both by svd and by a fully homotopy continuation approach
+
         assert len(solutions) >= 2, "No intersection points found"
-        
+
         II = solutions[0]
         JJ = solutions[1]
         self.logger.info(f"II: {II}")
         self.logger.info(f"JJ: {JJ}")
-        
+
         # Compute the dual conic of the circular points
         imDCCP = np.outer(II, JJ) + np.outer(JJ, II)
 
@@ -78,8 +82,7 @@ class HomotopyContinuationRectifier(Rectifier):
         H = self._compute_h_from_svd(imDCCP)
 
         return H
-        
-    
+
     def _get_script(self, filename: str) -> str:
         path = os.path.join(os.path.dirname(__file__), filename)
         script = open(path, "r").read()

src/HomoTopiContinuation/Rectifier/rectifier.py
@@ -4,12 +4,14 @@ import logging
 import numpy as np
 import numpy.linalg as la
 
+
 class Rectifier(ABC):
     """
     Abstract base class for rectification algorithms.
 
     This class defines the interface that all rectifiers must implement.
     """
+
     def __init__(self, treshold: float = 1e-3):
         self.treshold = treshold
         logging.basicConfig(
@@ -40,20 +42,16 @@ class Rectifier(ABC):
         """
         Compute the Homography from the SVD of the image dual conic.
         """
-        eigs = np.linalg.eigvals(imDCCP)
-
-        # thresholding
-        eigs[np.abs(eigs) < self.treshold] = 0
-
-        if np.any(eigs < 0):
-            self.logger.error("imDCCP is not positive definite")
-            raise ValueError(
-                f"imDCCP is not positive definite! No homography can be computed. eigs: {eigs}")
 
         self.logger.info(f"imDCCP\n: {imDCCP}")
 
         # Singular value decomposition
         U, S, Vt = la.svd(imDCCP)
+        if np.any(S < 0):
+            self.logger.error("imDCCP is not positive definite")
+            raise ValueError(
+                f"imDCCP is not positive definite! No homography can be computed. Singular values: {S}")
+
         self.logger.info(f"U\n: {U}")
         self.logger.info(f"S\n: {S}")
         self.logger.info(f"V\n: {Vt}")

src/HomoTopiContinuation/Rectifier/standard_rectifier.py
@@ -87,8 +87,8 @@ class StandardRectifier(Rectifier):
         self.logger.info(f"JJ: {JJ}")
 
         # Compute the dual conic of the circular points
-        imDCCP = np.outer(II, JJ.T) + np.outer(JJ, II.T)
-        imDCCP = imDCCP / la.norm(imDCCP)
+        imDCCP = II @ JJ.T + JJ @ II.T
+        # imDCCP = imDCCP / la.norm(imDCCP)
 
         H = self._compute_h_from_svd(imDCCP)
 

src/HomoTopiContinuation/experiment.py
@@ -3,12 +3,13 @@ import numpy as np
 import HomoTopiContinuation.Plotter.Plotter as Plotter
 import HomoTopiContinuation.SceneGenerator.scene_generator as sg
 import HomoTopiContinuation.Rectifier.standard_rectifier as sr
+# import HomoTopiContinuation.Rectifier.homotopyc_rectifier as hr
 
 
 def sceneDefinition() -> sg.SceneDescription:
     # Parameters
     f = 1
-    theta = 20
+    theta = 0
 
     # Define the circles
     c1 = Circle(np.array([0, 0]), 1)
@@ -27,6 +28,7 @@ def main():
     print("[Scene Generated]")
 
     rectifier = sr.StandardRectifier()
+    # rectifier = hr.HomotopyContinuationRectifier()
     try:
         H_reconstructed = rectifier.rectify(img.C_img)
     except ValueError as e: