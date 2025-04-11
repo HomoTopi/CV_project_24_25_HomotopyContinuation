b2610afd003f2db528f16adca94e4fb3df3de6ac
Martina Missana <martina.missana@mail.polimi.it>
2025-04-11 16:52:09 +0200

    feat: add i/o  through json files in rectifiers

--- src/HomoTopiContinuation/Rectifier/homotopyc_rectifier.py
+++ src/HomoTopiContinuation/Rectifier/homotopyc_rectifier.py
@@ -3,10 +3,10 @@ import logging
 from juliacall import Main as jl
 import juliapkg
 import os
+import json
 
-
-from HomoTopiContinuation.DataStructures.datastructures import Conics, Homography
-from .rectifier import Rectifier
+from HomoTopiContinuation.DataStructures.datastructures import Conics, Homography, Img
+from HomoTopiContinuation.Rectifier.rectifier import Rectifier
 
 
 class HomotopyContinuationRectifier(Rectifier):
@@ -29,53 +29,54 @@ class HomotopyContinuationRectifier(Rectifier):
         # call the __init__ of the parent class
         super().__init__()
 
-    def rectify(self, C_img: Conics) -> Homography:
+    def rectify(self):
         """
         Rectify a pair of conics using the Julia Homotopy Continuation package.
-
-        Args:
-            C_img (Conics): A pair of conics in the image
 
         Returns:
             Homography: The rectification homography
         """
+        with open("src\\HomoTopiContinuation\\Data\\sceneImage.json", "r") as file:
+            data = json.load(file)  # This is now a list of Img JSONs
 
-        script = self._get_script('Julia/rectify.jl')
-        self.logger.info("Rectifying using HomotopyContinuation.jl")
-        self.logger.debug(f"Conics: {C_img}")
+        rectified_homographies = []
 
-        self.logger.info(f"Evaluating script: {script}")
+        for img_json in data:
+            img = Img.from_json(img_json)
+            C_img = img.C_img
 
-        # Extract algebraic parameters from conics
-        a1, b1, c1, d1, e1, f1 = C_img.C1.to_algebraic_form()
-        a2, b2, c2, d2, e2, f2 = C_img.C2.to_algebraic_form()
-        a3, b3, c3, d3, e3, f3 = C_img.C3.to_algebraic_form()
+            script = self._get_script('Julia/rectify.jl')
+            self.logger.info("Rectifying using HomotopyContinuation.jl")
+            self.logger.debug(f"Conics: {C_img}")
+            self.logger.info(f"Evaluating script: {script}")
 
-        self.logger.info(
-            f"Equation 1: {a1}*x^2 + {b1}*x*y + {c1}*y^2 + {d1}*x*w + {e1}*y*w + {f1}*w^2")
-        self.logger.info(
-            f"Equation 2: {a2}*x^2 + {b2}*x*y + {c2}*y^2 + {d2}*x*w + {e2}*y*w + {f2}*w^2")
-        self.logger.info(
-            f"Equation 3: {a3}*x^2 + {b3}*x*y + {c3}*y^2 + {d3}*x*w + {e3}*y*w + {f3}*w^2")
+            # Extract algebraic parameters from conics
+            a1, b1, c1, d1, e1, f1 = C_img.C1.to_algebraic_form()
+            a2, b2, c2, d2, e2, f2 = C_img.C2.to_algebraic_form()
+            a3, b3, c3, d3, e3, f3 = C_img.C3.to_algebraic_form()
 
-        jl.a1, jl.b1, jl.c1, jl.d1, jl.e1, jl.f1 = a1, b1, c1, d1, e1, f1
-        jl.a2, jl.b2, jl.c2, jl.d2, jl.e2, jl.f2 = a2, b2, c2, d2, e2, f2
-        jl.a3, jl.b3, jl.c3, jl.d3, jl.e3, jl.f3 = a3, b3, c3, d3, e3, f3
+            self.logger.info(f"Equation 1: {a1}*x^2 + {b1}*x*y + {c1}*y^2 + {d1}*x*w + {e1}*y*w + {f1}*w^2")
+            self.logger.info(f"Equation 2: {a2}*x^2 + {b2}*x*y + {c2}*y^2 + {d2}*x*w + {e2}*y*w + {f2}*w^2")
+            self.logger.info(f"Equation 3: {a3}*x^2 + {b3}*x*y + {c3}*y^2 + {d3}*x*w + {e3}*y*w + {f3}*w^2")
 
-        jl.seval(script)
-        # TODO: check if casting is the same as the one used in SymPy
-        solutions = np.array([[complex(sol) for sol in sol_tuple]
-                              for sol_tuple in jl.complex_sols])
-        # real_solutions = np.array([ np.float64(sol) for sol in jl.real_sol])
-        self.logger.info(f"Result: {solutions}")
-        # self.logger.info(f"Real solutions: {real_solutions}")
-        # TODO: extract the intersection points from the result
-        # TODO: compute the rectification homography both by svd and by a fully homotopy continuation approach
+            jl.a1, jl.b1, jl.c1, jl.d1, jl.e1, jl.f1 = a1, b1, c1, d1, e1, f1
+            jl.a2, jl.b2, jl.c2, jl.d2, jl.e2, jl.f2 = a2, b2, c2, d2, e2, f2
+            jl.a3, jl.b3, jl.c3, jl.d3, jl.e3, jl.f3 = a3, b3, c3, d3, e3, f3
 
-        imDCCP = self.compute_imDCCP_from_solutions(solutions)
-        H = self._compute_h_from_svd(imDCCP)
+            jl.seval(script)
 
-        return H
+            solutions = np.array([[complex(sol) for sol in sol_tuple] for sol_tuple in jl.complex_sols])
+            self.logger.info(f"Result: {solutions}")
+
+            imDCCP = self.compute_imDCCP_from_solutions(solutions)
+            H = self._compute_h_from_svd(imDCCP)
+
+            # Save rectified homography JSON
+            rectified_homographies.append(H.to_json())
+
+        with open('src\\HomoTopiContinuation\\Data\\rectifiedHomography.json', 'w') as file:
+            json.dump(rectified_homographies, file, indent=4)
+
 
     def _get_script(self, filename: str) -> str:
         """"
@@ -89,3 +90,7 @@ class HomotopyContinuationRectifier(Rectifier):
         path = os.path.join(os.path.dirname(__file__), filename)
         script = open(path, "r", encoding="utf8").read()
         return script
+
+if __name__ == "__main__":
+    rectifier = HomotopyContinuationRectifier()
+    rectifier.rectify()
--- src/HomoTopiContinuation/Rectifier/standard_rectifier.py
+++ src/HomoTopiContinuation/Rectifier/standard_rectifier.py
@@ -1,11 +1,11 @@
 import numpy as np
 from numpy import linalg as la
-import logging
+import json
 from sympy import symbols, Eq, solve
 
 
-from HomoTopiContinuation.DataStructures.datastructures import Conics, Homography
-from .rectifier import Rectifier
+from HomoTopiContinuation.DataStructures.datastructures import Conics, Homography, Img
+from HomoTopiContinuation.Rectifier.rectifier import Rectifier
 
 
 class StandardRectifier(Rectifier):
@@ -16,55 +16,74 @@ class StandardRectifier(Rectifier):
     points, then computes the homography.
     """
 
-    def rectify(self, C_img: Conics) -> Homography:
+    def rectify(self):
         """
         Rectify a pair of conics using SymPy.
-
-        Args:
-            C_img (Conics): A pair of conics in the image
 
         Returns:
             Homography: The rectification homography
         """
-        self.logger.info("Rectifying using scipy")
+        with open("src\\HomoTopiContinuation\\Data\\sceneImage.json", "r") as file:
+            data = json.load(file)  # This is now a list of Img JSONs
 
-        self.logger.info(f"Conics: {C_img}")
-
-        # Extract algebraic parameters from conics
-        a1, b1, c1, d1, e1, f1 = C_img.C1.to_algebraic_form()
-        a2, b2, c2, d2, e2, f2 = C_img.C2.to_algebraic_form()
-        a3, b3, c3, d3, e3, f3 = C_img.C3.to_algebraic_form()
-
-        self.logger.info(
-            f"Equation 1: {a1}*x^2 + {b1}*x*y + {c1}*y^2 + {d1}*x*w + {e1}*y*w + {f1}*w^2")
-        self.logger.info(
-            f"Equation 2: {a2}*x^2 + {b2}*x*y + {c2}*y^2 + {d2}*x*w + {e2}*y*w + {f2}*w^2")
-        self.logger.info(
-            f"Equation 3: {a3}*x^2 + {b3}*x*y + {c3}*y^2 + {d3}*x*w + {e3}*y*w + {f3}*w^2")
+        rectified_homographies = []
 
         x, y, w = symbols('x y w')
-        eq = [Eq(a1*x**2 + b1*x*y + c1*y**2 + d1*x*w + e1*y*w + f1*w**2, 0),
-              Eq(a2*x**2 + b2*x*y + c2*y**2 + d2*x*w + e2*y*w + f2*w**2, 0),
-              Eq(a3*x**2 + b3*x*y + c3*y**2 + d3*x*w + e3*y*w + f3*w**2, 0)]
 
-        # Solve the system of equations
-        # TODO filter real solutions
+        for img_json in data:
+            img = Img.from_json(img_json)
+            C_img = img.C_img
+            self.logger.info("Rectifying using scipy")
 
-        solutions = solve(eq, (x, y, w))
-        self.logger.debug(f"Result of solve: {solutions}")
+            self.logger.info(f"Conics: {C_img}")
 
-        # Extract complex solutions
-        sols = np.array([[complex(expr.subs({x: 1, y: 1, w: 1}))
-                        for expr in expr_tuple] for expr_tuple in solutions])
-        self.logger.info(f"Solutions before filtering: {sols}")
+            # Extract algebraic parameters from conics
+            a1, b1, c1, d1, e1, f1 = C_img.C1.to_algebraic_form()
+            a2, b2, c2, d2, e2, f2 = C_img.C2.to_algebraic_form()
+            a3, b3, c3, d3, e3, f3 = C_img.C3.to_algebraic_form()
 
-        if len(sols) == 0:
-            self.logger.error("No solutions found")
-            raise ValueError(
-                f"No solutions found! sols: {sols}")
-            
-            
-        imDCCP = self.compute_imDCCP_from_solutions(sols)
-        H = self._compute_h_from_svd(imDCCP)
+            self.logger.info(
+                f"Equation 1: {a1}*x^2 + {b1}*x*y + {c1}*y^2 + {d1}*x*w + {e1}*y*w + {f1}*w^2")
+            self.logger.info(
+                f"Equation 2: {a2}*x^2 + {b2}*x*y + {c2}*y^2 + {d2}*x*w + {e2}*y*w + {f2}*w^2")
+            self.logger.info(
+                f"Equation 3: {a3}*x^2 + {b3}*x*y + {c3}*y^2 + {d3}*x*w + {e3}*y*w + {f3}*w^2")
 
-        return H
+            eq = [Eq(a1*x**2 + b1*x*y + c1*y**2 + d1*x*w + e1*y*w + f1*w**2, 0),
+                Eq(a2*x**2 + b2*x*y + c2*y**2 + d2*x*w + e2*y*w + f2*w**2, 0),
+                Eq(a3*x**2 + b3*x*y + c3*y**2 + d3*x*w + e3*y*w + f3*w**2, 0)]
+
+            # Solve the system of equations
+            # TODO filter real solutions
+
+            solutions = solve(eq, (x, y, w))
+            self.logger.debug(f"Result of solve: {solutions}")
+
+            # Extract complex solutions
+            # TODO: check if casting is the same as the one used in SymPy
+            sols = np.array([[complex(expr.subs({x: 1, y: 1, w: 1}))
+                            for expr in expr_tuple] for expr_tuple in solutions])
+            self.logger.info(f"Solutions before filtering: {sols}")
+
+            if len(sols) == 0:
+                self.logger.error("No solutions found")
+                raise ValueError(
+                    f"No solutions found! sols: {sols}")
+
+            imDCCP = self.compute_imDCCP_from_solutions(sols)
+            # TODO: compute the rectification homography both by svd and by a fully homotopy continuation approach
+            H = self._compute_h_from_svd(imDCCP)
+
+            rectified_homographies.append(H.to_json())
+
+        # write the list of rectified homographies to a file
+        with open('src\\HomoTopiContinuation\\Data\\rectifiedHomography.json', 'w') as file:
+            json.dump(rectified_homographies, file, indent=4)
+
+
+if __name__ == "__main__":
+    # Create a StandardRectifier instance
+    rectifier = StandardRectifier()
+
+    # Call the rectify method to test it
+    rectifier.rectify()
