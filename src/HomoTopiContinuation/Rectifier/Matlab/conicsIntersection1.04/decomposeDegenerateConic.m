ff8eed781725f6b104ba129c925ffa8befa05af2
filomba01 <f.balzarini@an4a.com>
2025-04-16 16:36:38 +0200

    feat: the numeric rectifier now is implemented in matlab

---
+++ src/HomoTopiContinuation/Rectifier/Matlab/conicsIntersection1.04/decomposeDegenerateConic.m
@@ -0,0 +1,42 @@
+% decomposeDegenerateConic -  decompose in two lines the given degenerate
+% conic (including complex decompositions)
+%              - Pierluigi Taddei (pierluigi.taddei@polimi.it)
+%
+% Usage:     [l m] = decomposeDegenerateConic(C)
+%
+% Arguments:
+%           c - degenerate symmetric 3x3 matrix of the conic
+%           l m - homogeneous line coordinates
+% 07.3.2007 : Created
+%
+%
+function [l m] = decomposeDegenerateConic(c)
+
+if (rank(c) == 1) %c is rank 1: direct split is possible
+    C = c;
+else %rank 2: need to detect the correct rank 1 matrix
+    %% use the dual conic of c
+    B = -adjointSym3(c);
+
+    %% detect intersection point p
+    [maxV di] = max(abs(diag(B)));
+    i = di(1);
+    
+    % Remove the B(i,i) < 0 check to allow complex decompositions
+    b = sqrt(B(i,i));  % This will be complex when B(i,i) < 0
+    p = B(:,i)/b;
+
+    %% detect lines product
+    Mp = crossMatrix(p);
+
+    C = c + Mp;
+end
+%% recover lines
+[maxV ci] = max(abs(C(:)));
+j = floor((ci(1)-1) / 3)+1;
+i = ci(1) - (j-1)*3;
+
+l = C(i,:)';
+m = C(:,j);
+
+end
--- tests/Rectifier/test_numeric_rectifier.py
+++ tests/Rectifier/test_numeric_rectifier.py
@@ -4,57 +4,75 @@ from HomoTopiContinuation.DataStructures.datastructures import Conic, Circle
 import numpy as np
 
 
-
-def test_compute_conic_line_intersection():
+def test_rectify():
     # create a numeric_rectifier
     numeric_rectifier = NumericRectifier()
 
     # create a conic
     c1 = Circle(np.array([0, 0]), 1)
     conic = c1.to_conic()
-    # create a linealpha * (lamb **3) + beta * (lamb **2) * mu + gamma * lamb * (mu ** 2) + delta * (mu ** 3)
-    # y = - x + 1
-    line = np.array([1, 1, -1])
-
-    intersection_points = numeric_rectifier._compute_conic_line_intersection(conic, line)
-
-    assert abs(intersection_points[0].T @ conic.M @ intersection_points[0]) < 1e-3
-    assert abs(intersection_points[1].T @ conic.M @ intersection_points[1]) < 1e-3
-
-
-def test_compute_conics_intersection():
-    # create a numeric_rectifier
-    numeric_rectifier = NumericRectifier()
-
-    # create two conics
-    c1 = Circle(np.array([0, 0]), 1)
-    c2 = Circle(np.array([1, 1]), 5)
-    conic1 = c1.to_conic()
-    conic2 = c2.to_conic()
-
-    result = numeric_rectifier._compute_conics_intersection(conic1, conic2)
-    print("result: ", result)
-    assert result.size > 0
-    for pt in result:
-        print(pt.T @ conic1.M @ pt)
-        print(pt.T @ conic2.M @ pt)
-        assert abs(pt.T @ conic1.M @ pt) < 1e-3
-        assert abs(pt.T @ conic2.M @ pt) < 1e-3
-
-def test_cubic_equation_solver():
-    numeric_rectifier = NumericRectifier()
-    c1 = Circle(np.array([0, 0]), 1)
-    c2 = Circle(np.array([1, 1]), 2)
-    conic1 = c1.to_conic()
-    conic1.M[0,0] = 2
+    c2 = Circle(np.array([3, 3]), 1)
     conic2 = c2.to_conic()
     
-    
-    
-    alpha, beta, gamma, delta = numeric_rectifier._get_parameters(conic1, conic2)
-    #lamb, mu = numeric_rectifier._compute_cubic_equation(alpha, beta,gamma,delta)
-    lamb, mu = numeric_rectifier._solve_symbolic_cubic_equation(alpha, beta,gamma,delta)
-    lamb = lamb[0]
-    mu = mu[0]
-    res = alpha * (lamb **3) + beta * (lamb **2) * mu + gamma * lamb * (mu ** 2) + delta * (mu ** 3)
-    assert abs(res) < 10e-3
+    c3 = Circle(np.array([2, 2]), 3)
+    conic3 = c3.to_conic()
+
+    Conics = [conic, conic2, conic3]
+    # rectify the conics
+    homography = numeric_rectifier.rectify(Conics)
+    print(homography)
+    assert False
+#
+#def test_compute_conic_line_intersection():
+#    # create a numeric_rectifier
+#    numeric_rectifier = NumericRectifier()
+#
+#    # create a conic
+#    c1 = Circle(np.array([0, 0]), 1)
+#    conic = c1.to_conic()
+#    # create a linealpha * (lamb **3) + beta * (lamb **2) * mu + gamma * lamb * (mu ** 2) + delta * (mu ** 3)
+#    # y = - x + 1
+#    line = np.array([1, 1, -1])
+#
+#    intersection_points = numeric_rectifier._compute_conic_line_intersection(conic, line)
+#
+#    assert abs(intersection_points[0].T @ conic.M @ intersection_points[0]) < 1e-3
+#    assert abs(intersection_points[1].T @ conic.M @ intersection_points[1]) < 1e-3
+#
+
+#def test_compute_conics_intersection():
+#    # create a numeric_rectifier
+#    numeric_rectifier = NumericRectifier()
+#
+#    # create two conics
+#    c1 = Circle(np.array([0, 0]), 1)
+#    c2 = Circle(np.array([1, 1]), 5)
+#    conic1 = c1.to_conic()
+#    conic2 = c2.to_conic()
+#
+#    result = numeric_rectifier._compute_conics_intersection(conic1, conic2)
+#    print("result: ", result)
+#    assert result.size > 0
+#    for pt in result:
+#        print(pt.T @ conic1.M @ pt)
+#        print(pt.T @ conic2.M @ pt)
+#        assert abs(pt.T @ conic1.M @ pt) < 1e-3
+#        assert abs(pt.T @ conic2.M @ pt) < 1e-3
+#
+#def test_cubic_equation_solver():
+#    numeric_rectifier = NumericRectifier()
+#    c1 = Circle(np.array([0, 0]), 1)
+#    c2 = Circle(np.array([1, 1]), 2)
+#    conic1 = c1.to_conic()
+#    conic1.M[0,0] = 2
+#    conic2 = c2.to_conic()
+#    
+#    
+#    
+#    alpha, beta, gamma, delta = numeric_rectifier._get_parameters(conic1, conic2)
+#    #lamb, mu = numeric_rectifier._compute_cubic_equation(alpha, beta,gamma,delta)
+#    lamb, mu = numeric_rectifier._solve_symbolic_cubic_equation(alpha, beta,gamma,delta)
+#    lamb = lamb[0]
+#    mu = mu[0]
+#    res = alpha * (lamb **3) + beta * (lamb **2) * mu + gamma * lamb * (mu ** 2) + delta * (mu ** 3)
+#    assert abs(res) < 10e-3
